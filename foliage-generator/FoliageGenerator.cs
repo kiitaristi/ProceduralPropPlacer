35145fec760f1bd68dacc1d931b760632a9dbe3c
jade ! <150134796+kiitaristi@users.noreply.github.com>
2026-02-05 06:48:06 -0800

    Push project to remote

---
+++ foliage-generator/FoliageGenerator.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+[Tool]
+public partial class FoliageGenerator : Node3D
+{
+	private bool _toolActive = false;
+	
+	public override void _Ready() {
+		// process that occurs at runtime
+		if (!Engine.IsEditorHint()) {
+			_toolActive = false;
+			// FIXME: what the attached foliage tool 
+			// outputs from the parent object's transform
+		}
+	}
+	
+	public override void _Process(double delta) {
+		// process that occurs in editor
+		if (Engine.IsEditorHint()) {
+			// FIXME: foliage tool output preview
+		}
+	}
+}
