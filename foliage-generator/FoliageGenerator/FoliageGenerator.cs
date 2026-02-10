8490709d935ff7b3ecf1e4abd6c2ccb93690092d
jade ! <kiitaristi@gmail.com>
2026-02-10 05:10:27 -0800

    Add noise parameters, tool settings, in-editor functionality

--- foliage-generator/FoliageGenerator/FoliageGenerator.cs
+++ foliage-generator/FoliageGenerator/FoliageGenerator.cs
@@ -4,16 +4,24 @@ using System;
 [Tool]
 public partial class FoliageGenerator : Node3D
 {
-	private bool _toolActive = false;
-	private GodotObject discSample; 
+	private GodotObject discSample;
+	private bool _test = true;
+	private Vector3 _scalePrev;
 	
-	[ExportCategory("Mesh Settings")]
-	[Export] public MeshInstance3D meshObject;
+	[ExportCategory("Active Editor Updates")]
+	[Export] public bool toolActive = false;
+	
+	[ExportCategory("Object Settings")]
+	[Export] public GodotObject objectMesh;
 	[Export(PropertyHint.Range, "0,10,")]
-	public float meshScale = 1f;
+	public float objectXScale = 1f;
+	[Export(PropertyHint.Range, "0,10,")]
+	public float objectYScale = 1f;
+	[Export(PropertyHint.Range, "0,10,")]
+	public float objectZScale = 1f;
 	
-	[ExportCategory("Noise Settings")]
-	[Export] public FastNoiseLite noiseMap;
+	[ExportCategory("Generation Settings")]
+	[Export] public FastNoiseLite noise { get; set; }
 	[Export] public int maximumObjects;
 	
 	[ExportCategory("Seed")]
@@ -21,8 +29,13 @@ public partial class FoliageGenerator : Node3D
 	
 	public override void _Ready() {
 		// process that occurs at runtime
+		if (Engine.IsEditorHint()) {
+			noise.Offset = this.GetPosition();
+			_scalePrev = this.GetScale();
+		}
+		
 		if (!Engine.IsEditorHint()) {
-			_toolActive = false;
+			toolActive = false;
 			// FIXME: what the attached foliage tool 
 			// outputs from the parent object's transform
 		}
@@ -30,12 +43,19 @@ public partial class FoliageGenerator : Node3D
 	
 	public override void _Process(double delta) {
 		// process that occurs in editor
-		if (Engine.IsEditorHint()) {
-			
+		if (Engine.IsEditorHint() && toolActive) {
+			if (noise.Offset != this.GetPosition() || _scalePrev != this.GetScale()) { 
+				noise.Offset = this.GetPosition();
+				_scalePrev = this.GetScale();
+				
+				GD.Print("new pos: " + this.GetPosition() + ", new scale: " + this.GetScale());
+			}
 		}
 	}
 	
 	private void _InstantiateSampling() {
-		var scriptPath = GD.Load<GDScript>();
+		var scriptPath = GD.Load<GDScript>(
+			"res://addons/PoissonDiscSampling/poisson_disc_sampling.gd");	
+		discSample = (GodotObject)scriptPath.New();
 	}
 }
